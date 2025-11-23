0b7d12db06a5111081184235cf7eb5a3aa983139
ghulam haider <ghulamhaider11201@gmail.com>
2025-11-23 13:37:06 +0500

    Complete Hero Sectiom

--- src/componants/HeroSection.jsx
+++ src/componants/HeroSection.jsx
@@ -1,44 +1,76 @@
 import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 
-function HeroSection() {
-  const [isHovered, setIsHovered] = useState(false);
+import { motion } from 'framer-motion';
 
+
+import profileImage from "../assets/profileImage.jpeg";
+
+function HeroSection() {
   return (
-    <div>
-      <div>
-        <h1>Ghulam Haider</h1>
-      </div>
-      <div className="flex">
-        <h1 className="text-2xl typewriter">
-          I am a <span className="text-accent">freelancer</span>
-        </h1>
-      </div>
-      <div className="flex gap-5 mt-4">
-        <Link
-          to="#"
-          onMouseEnter={() => setIsHovered(true)}
-          onMouseLeave={() => setIsHovered(false)}
-          
-          className={` ${
-           isHovered ? 'btnTransparent' : 'btn'
-          }`}
+    <>
+      <motion.div
+        initial={{ y: 20, opacity: 0 }}
+        animate={{ y: 0, opacity: 1 }}
+        transition={{ duration: 0.8, ease: "easeOut" }}
+        className='w-full mb-4 mt-28 md:mt-36 lg:mt-0 md:h-screen lg:flex gap-9 md:gap-0 justify-center items-center'
+
+      >
+        {/* // left side contant */}
+        <div className='flex gap-8 mt-5 lg:mt-0  justify-center items-center p-6 md:p-0 md:pl-12  w-full lg:w-[50%] flex-col'>
+          <div className="flex w-full  items-center lg:items-start ">
+            <h1 className='flex justify-center text-2xl md:text-3xl w-full  lg:text-4xl xl:text-5xl'>Hi, I'M Ghulam Haider</h1>
+          </div>
+          <div className="flex flex-col w-full gap-5  items-center lg:items-start  lg:w-[75%]">
+            <h1 className=" md:text-2xl lg:text-3xl xl:text-4xl typewriter">
+              Full Stack <span className="text-accent">Developer</span>
+            </h1>
+            <p className='w-full md:w-[75%]  lg:w-full'>
+              "Crafting interactive front-end experiences and building efficient back-end architectures, I turn ideas into reliable, modern web applications."
+            </p>
+          </div>
+          <div className="flex gap-6 mt-4">
+            <Link
+              to="#"
+              className='btn hover:bg-accent transition-colors duration-300 hover:text-black '
+            >
+              Hire Me
+            </Link>
+            <Link
+              to="#"
+              className='btnTransparent hover:bg-accent transition-colors duration-300 hover:text-black'
+            >
+              About Me
+            </Link>
+          </div>
+        </div>
+
+        {/* Right Side Contant */}
+        <div
+          className=' flex  justify-center items-center lg:pl-12 lg:w-[50%]  mt-10 lg:mt-0'
         >
-          Hire Me
-        </Link>
-        <Link
-          to="#"
-           onMouseEnter={() => setIsHovered(true)}
-           onMouseLeave={() => setIsHovered(false)}
-           className={` ${
-            isHovered ? 'btn' : 'btnTransparent'
-          }`}
-       
-        >
-          About Me
-        </Link>
-      </div>
-    </div>
+          <div className='w-[290px] h-[290px] md:w-[325px] md:h-[325px] relative bg-accent rounded-full'>
+            <motion.div
+        initial={{ y: 0, }}
+       animate={{ y: [-10, 0, -10] }}
+        transition={{ 
+          duration: 3,    
+          repeat: Infinity, 
+          repeatType: "loop",
+           ease: "easeInOut"  
+        }}
+      
+            
+            >
+             <img className='w-[290px] h-[290px] md:w-[325px] md:h-[325px] rounded-full ' src={profileImage} alt="profile" />
+            </motion.div>
+
+
+          </div>
+        </div>
+      </motion.div>
+
+    </>
   );
 }
 
--- src/componants/Navbar.jsx
+++ src/componants/Navbar.jsx
@@ -1,17 +1,41 @@
-import React from 'react'
-import { Link, NavLink } from 'react-router-dom'
+import React, { useState, useEffect} from "react";
+import { Link, NavLink } from "react-router-dom";
+import { HiMenu, HiX } from "react-icons/hi"
 
 function Navbar() {
+   const [open, setOpen] = useState(false);
+  
+   const [scrolled, setScrolled] = useState(false);
+
+
+
+useEffect(() => {
+    const handleScroll = () => {
+      if (window.scrollY > 50) {
+        setScrolled(true);
+      } else {
+        setScrolled(false);
+      }
+    };
+
+    window.addEventListener("scroll", handleScroll);
+
+    return () => window.removeEventListener("scroll", handleScroll);
+  }, []);
+
   return (
     <>
-      <div className='contianer flex justify-between p-4'>
+      <div className={`sticky top-0 z-50 transition-colors duration-500 ${
+        scrolled ? "bg-[#0f0f0f]" : "bg-transparent"
+      }`}>
+        <div className='contianer flex justify-between p-4'>
         <div>
-          <h1>
-            Coder 
+          <h1 className="text-2xl">
+            Code 
             <span className='text-accent'>@Me</span>
           </h1>
         </div>
-        <nav className='flex gap-5 '>
+        <nav className='hidden md:flex gap-5 '>
          <NavLink
   to="/"
   className={({ isActive }) =>
@@ -57,8 +81,78 @@ function Navbar() {
   Contact
 </NavLink>
         </nav>
+        
 
+     
+
+ {/* Mobile Hamburger */}
+        <button
+          className="md:hidden text-3xl"
+          onClick={() => setOpen(!open)}
+        >
+         {open ? <HiX /> : <HiMenu />}
+        </button>
       </div>
+      </div>
+
+      {/* Mobile Menu */}
+        <div>
+           <nav className={`flex flex-col items-center gap-7 h-screen bg-[#0f0f0f] text-white md:hidden transition-all duration-300 ${
+    open ? "flex" : "hidden"
+  }`}>
+         <NavLink
+  to="/"
+  
+  className={({ isActive }) =>
+    `relative inline-block transition-all duration-300 underline-offset-4
+     after:content-[''] after:absolute after:left-0 after:-bottom-1 
+     after:h-[2px] after:w-0 after:bg-accent after:transition-all after:duration-300
+     ${isActive ? "text-accent after:w-full" : "hover:text-accent hover:after:w-full"}`
+     
+  }
+    onClick={() => setOpen(!open)}
+>
+  Home
+</NavLink>
+         <NavLink
+  to="services"
+  className={({ isActive }) =>
+    `relative inline-block transition-all duration-300 underline-offset-4
+     after:content-[''] after:absolute after:left-0 after:-bottom-1 
+     after:h-[2px] after:w-0 after:bg-accent after:transition-all after:duration-300
+     ${isActive ? "text-accent after:w-full" : "hover:text-accent hover:after:w-full"}`
+  }
+    onClick={() => setOpen(!open)}
+>
+  Services
+</NavLink>
+         <NavLink
+  to="about"
+  className={({ isActive }) =>
+    `relative inline-block transition-all duration-300 underline-offset-4
+     after:content-[''] after:absolute after:left-0 after:-bottom-1 
+     after:h-[2px] after:w-0 after:bg-accent after:transition-all after:duration-300
+     ${isActive ? "text-accent after:w-full" : "hover:text-accent hover:after:w-full"}`
+  }
+    onClick={() => setOpen(!open)}
+>
+  About
+</NavLink>
+         <NavLink
+  to="contact"
+  className={({ isActive }) =>
+    `relative inline-block transition-all duration-300 underline-offset-4
+     after:content-[''] after:absolute after:left-0 after:-bottom-1 
+     after:h-[2px] after:w-0 after:bg-accent after:transition-all after:duration-300
+     ${isActive ? "text-accent after:w-full" : "hover:text-accent hover:after:w-full"}`
+  }
+    onClick={() => setOpen(!open)}
+>
+  Contact
+</NavLink>
+        </nav>
+        </div>
+        
     </>
   )
 }
